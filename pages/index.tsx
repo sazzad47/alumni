327b424da6075b8a9c33ecf7f2fead34ae77d588
Sazzad <sazzad47.ju@gmail.com>
2022-12-27 18:29:30 -0800

    make it better

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,14 +9,14 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>BTRI School Ex-Students Forum</title>
+        <title>BTRI School Alumni Association</title>
         <meta name="description" content="Under development by Sazzad Hossen" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
         <div style={{display: 'flex', flexDirection: "column", alignItems: 'center', gap: "2rem"}}>
-         <h1>BTRI School Ex-Students Forum</h1>
+         <h1>BTRI School Alumni Association</h1>
          <h4>Under development by Sazzad Hossen</h4>
         </div>
       </main>
