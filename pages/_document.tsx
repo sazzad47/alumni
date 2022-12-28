e63d5e6227326dd4c83b970d61ff9c4cb7406175
Sazzad <sazzad47.ju@gmail.com>
2022-12-28 12:02:22 -0800

    make it better

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,4 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script'
 
 export default function Document() {
   return (
@@ -14,17 +13,18 @@ export default function Document() {
             href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
             rel="stylesheet"
           />
-          <Script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></Script>
-          <Script src="https://kit.fontawesome.com/a076d05399.js"></Script>
-          <Script src="https://cdn.syncfusion.com/ej2/material.css"></Script>
+          <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" async ></script>
+          <script src="https://kit.fontawesome.com/a076d05399.js" async ></script>
+          <script src="https://cdn.syncfusion.com/ej2/material.css" async ></script>
           <link
             href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
             rel="stylesheet"
           />
 
-          <Script
+          <script
             src={`https://www.paypal.com/sdk/js?client-id=${process.env.PAYPAL_CLIENT_ID}`}
-          ></Script>
+            async
+          ></script>
         </Head>
       <body>
         <Main />
