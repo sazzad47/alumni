34dcee90da131687db80ca226cb6082f53294c97
Sazzad <sazzad47.ju@gmail.com>
2023-01-01 11:08:00 -0800

    make it better

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const nextConfig = {
   env: {
-     "BASE_URL": process.env.NODE_ENV==='production'? "https://alumni-pied.vercel.app/": "http://localhost:3000",
+     "BASE_URL": process.env.NODE_ENV==='production'? "https://alumni-pied.vercel.app": "http://localhost:3000",
      "MONGODB_URL": process.env.MONGODB_URL,
      "SENDER_EMAIL": process.env.SENDER_EMAIL,
      "SENDER_EMAIL_PASSWORD": process.env.SENDER_EMAIL_PASSWORD,
