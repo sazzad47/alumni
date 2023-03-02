f4a40d7b113b36a5b2e26d014929b7b9192c9b63
Sazzad <sazzad47.ju@gmail.com>
2023-03-02 06:30:41 -0800

    make it better

--- components/news/detailed/index.tsx
+++ components/news/detailed/index.tsx
@@ -3,6 +3,7 @@ import React, { useContext, useState } from "react";
 import { Context, StoreProps } from "../../../store/store";
 import dynamic from "next/dynamic";
 import Parser from "html-react-parser";
+import Image from "next/image";
 
 const Update = dynamic(() => import("./update"), { ssr: false });
 
@@ -19,7 +20,6 @@ export interface Content {
 }
 const News = ({ data }: Content) => {
   const [updatedData, setUpdatedData] = useState(data);
-  console.log('updated', updatedData)
   const { state } = useContext(Context) as StoreProps;
   const { auth } = state;
 
@@ -37,10 +37,18 @@ const News = ({ data }: Content) => {
   return (
     <Grid className="w-full flex flex-col gap-5 py-[2rem]">
       <Grid className="w-full px-[2rem] flex items-center justify-between">
+        <Grid className="flex flex-col gap-3">
+        <Typography className="p-0 bold text-xl md:3xl">{updatedData.title}</Typography>
         <Typography className="p-0">{localDate(data?.createdAt)}</Typography>
+        </Grid>
         {auth?.user?.role === "admin" && (
           <Update data={updatedData} setUpdatedData={setUpdatedData} />
         )}
+      </Grid>
+      <Grid className="w-full md:w-1/2 px-[2rem] h-[10rem]">
+        <Grid className="w-full h-full relative">
+        <Image src={updatedData.photo} alt="photo" fill />
+        </Grid>
       </Grid>
       <Grid className="w-full max-w-full">
         {Parser(updatedData?.detailedPage)}
